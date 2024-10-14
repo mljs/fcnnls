cd624bf6f3fb07f16d6bb67658a79499d2704d6e
Luc Patiny <luc@patiny.com>
2024-10-14 16:29:14 +0200

    chore: update benchmark

--- benchmark/index.mjs
+++ benchmark/index.mjs
@@ -1,41 +1,46 @@
 /*
-Can be executed using `node -r esm --inspect-brk benchmark/index.js`
+Can be executed using `tsx --inspect-brk benchmark/index.js`
+Or `tsx --cpu-prof benchmark/index.js`
 And debug from chrome using `chrome://inspect`
 */
 
-import { readFileSync } from 'fs';
-import { join } from 'path';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 import { Matrix } from 'ml-matrix';
 
-import fcnnls from '../src/fcnnls';
+import { fcnnls } from '../src/fcnnls';
+
+const __dirname = join(new URL(import.meta.url).pathname, '..');
 
 const concentration = readFileSync(
   join(__dirname, '../src/__tests__/data/matrix.txt'),
-  'utf-8',
+  'utf8',
 );
 let linesA = concentration.split(/[\r\n]+/);
 let A = [];
 for (let line of linesA) {
-  A.push(line.split(',').map((value) => Number(value)));
+  A.push(line.split(',').map(Number));
 }
 let matrix = new Matrix(A);
 matrix = matrix.transpose();
 
 const observation = readFileSync(
   join(__dirname, '../src/__tests__/data/target.txt'),
-  'utf-8',
+  'utf8',
 );
 let lines = observation.split(/[\r\n]+/);
 let b = [];
 for (let line of lines) {
-  b.push(line.split(',').map((value) => Number(value)));
+  b.push(line.split(',').map(Number));
 }
 let target = new Matrix(b);
 target = target.transpose();
 
 console.profile('start');
 console.time('flag');
-let result = fcnnls(matrix, target);
+for (let i = 0; i < 20; i++) {
+  let result = fcnnls(matrix, target);
+}
 console.timeEnd('flag');
 console.profileEnd();
